33e87b968503af923391cf5a9ba9aefe125cb43e
rehcrr <vpereverzev1200@gmail.com>
2024-02-26 03:01:05 +0300

    added sh scripts in scripts folder

---
+++ scripts/data_download.sh
@@ -0,0 +1,9 @@
+pip install gdown
+mkdir data
+cd data
+#downloading all vacancies file.csv
+gdown 1EqHUBqc5oP2HbHr2gfHTwLJlyzrlp8Mt
+#downloading stop words
+gdown 1lzIormYOtkib58CBd3Q_SsnFSnqiMmwe && gdown 1-2Uz1-7wOXzH0lOC2meOHEFPPDPFgb3_
+#downloading cleaned data
+gdown 1lWsuFKJCe4y5JUi4WkU9lgY_lDBpO6Ub
---
+++ scripts/models_download.sh
@@ -0,0 +1,6 @@
+#downloading CatBoost model
+gdown 1fj_ssNcOQn32OrY5XSLeunes-xmxpqUQ
+#downloading LDA model
+gdown 1qR3u6vgGt2LoVYQ3Tfg3gMG9T_0HdoPn
+#downloading NMF model
+gdown 17IfEBovPJA5Nrvhg1QUgZvTiTKTAXWqh
